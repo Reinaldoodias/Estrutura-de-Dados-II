7d9ab8bf8cc7dc6ea381d482191f4e39d984052a
reinaldodias <108498807+Reinaldoodias@users.noreply.github.com>
2023-08-30 18:13:59 -0300

    agdsfg

---
+++ ReinaldoDias_ATV03_EDII/src/questao01/Run.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questao01;
+
+/**
+ *
+ * @author 20221SI0029
+ */
+public class Run {
+    public static void main(String[] args){
+        int list[] = {3,4,6,1,2,7,5,8,9,10};
+        Hibridosort.hibrido(list);
+    }
+}
